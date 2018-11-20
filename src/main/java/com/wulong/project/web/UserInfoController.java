63b10bff7093263ae698ca9ce3064228e13a2a48
wu491925129 <491925129@qq.com>
2018-11-20 10:12:11 +0800

    Initial commit

---
+++ src/main/java/com/wulong/project/web/UserInfoController.java
@@ -0,0 +1,56 @@
+package com.wulong.project.web;
+import com.wulong.project.core.Result;
+import com.wulong.project.core.ResultGenerator;
+import com.wulong.project.model.UserInfo;
+import com.wulong.project.service.UserInfoService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+* Created by CodeGenerator on 2018/11/19.
+*/
+@RestController
+@RequestMapping("/user/info")
+public class UserInfoController {
+    @Resource
+    private UserInfoService userInfoService;
+
+    @PostMapping("/add")
+    public Result add(UserInfo userInfo) {
+        userInfoService.save(userInfo);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/delete")
+    public Result delete(@RequestParam Integer id) {
+        userInfoService.deleteById(id);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/update")
+    public Result update(UserInfo userInfo) {
+        userInfoService.update(userInfo);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/detail")
+    public Result detail(@RequestParam Integer id) {
+        UserInfo userInfo = userInfoService.findById(id);
+        return ResultGenerator.genSuccessResult(userInfo);
+    }
+
+    @PostMapping("/list")
+    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        List<UserInfo> list = userInfoService.findAll();
+        PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+}
